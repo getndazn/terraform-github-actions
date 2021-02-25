60e3031a48071ba9d6870ab13e49578b9fbdf52c
James Peiris <jamespeiris@hotmail.com>
2021-02-25 15:55:19 +0000

    Compiled build and skeleton huskyrc

--- dist/index.js
+++ dist/index.js
@@ -1685,17 +1685,13 @@ function execTerraform() {
         const apply = core.getInput('apply');
         const destroy = core.getInput('destroy');
         const varFile = core.getInput('var_file');
+        const destroyTarget = core.getInput('destroy_target');
         // TF destroy
         if (destroy) {
-            if (varFile) {
-                if (shell.exec(`terraform destroy -var-file='${varFile}' --auto-approve`).code !== 0) {
-                    throw new Error(`unable to destroy terraform`);
-                }
-            }
-            else {
-                if (shell.exec('terraform destroy --auto-approve').code !== 0) {
-                    throw new Error(`unable to destroy terraform`);
-                }
+            const varCommand = varFile ? `-var-file='${varFile}` : '';
+            const destroyCommand = destroyTarget ? `-target='${destroyTarget}` : '';
+            if (shell.exec(`terraform destroy ${varCommand} --auto-approve ${destroyCommand}`).code !== 0) {
+                throw new Error(`unable to destroy terraform`);
             }
         }
         // TF plan
